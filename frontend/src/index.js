8a53ece0834218aec49723cf3d797131e123e691
swathi <swathibalasubramanyam@gmail.com>
2023-10-03 18:58:32 -0700

    completed frontend csrf auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import configureStore from './store';
+import csrfFetch, {restoreCSRF} from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production'){
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
-ReactDOM.render(
-  <React.StrictMode>
+const Root = () => {
+  return (
     <Provider store={store}>
       <BrowserRouter>
-      <App />
+        <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  )
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root></Root>
+    </React.StrictMode>,
   document.getElementById('root')
-);
+)};
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,29 @@
+
+export const restoreCSRF = async() => {
+    const response = await csrfFetch('/api/session');
+    storeCSRFToken(response);
+    return response;
+}
+
+export const storeCSRFToken = (response) => {
+    const csrfToken = response.headers.get("X-CSRF-Token");
+    if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
+}
+
+const csrfFetch = async(url, options={}) => {
+
+    options.headers ||= {}
+    options.method ||= 'GET'
+
+    if (options.method.toUpperCase() !== 'GET') {
+        options.headers['X-CSRF-Token'] = sessionStorage.getItem('X-CSRF-Token')
+        options.headers['Content-Type'] ||= 'application/json'
+    }
+
+    const res = await fetch(url, options);
+
+    if (res.status > 400) throw res;
+    return res;
+}
+
+export default csrfFetch;
